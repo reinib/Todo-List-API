874678680ecfd6ed252b20666eab34a4f40702dc
Brent Reininger <brent.reininger89@gmail.com>
2019-05-28 15:35:11 -0700

    Make Delete Button Work

--- public/app.js
+++ public/app.js
@@ -1,1 +1,58 @@
-alert("HI FROM APP.JS");
+$(document).ready(function() {
+  $.getJSON("/api/todos").then(addTodos);
+
+  $("#todoInput").keypress(function(event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
+});
+
+$(".list").on("click", "span", function() {
+  removeTodo($(this).parent());
+});
+
+function addTodos(todos) {
+  // add todos to page here
+  todos.forEach(function(todo) {
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo) {
+  var newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>");
+  newTodo.data("id", todo._id);
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $(".list").append(newTodo);
+}
+
+function createTodo() {
+  // send request to create new todo
+  var usrInput = $("#todoInput").val();
+  $.post("/api/todos", { name: usrInput })
+    .then(function(newTodo) {
+      console.log(newTodo);
+      $("#todoInput").val("");
+      addTodo(newTodo);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function removeTodo(todo) {
+  var clickedId = todo.data("id");
+  var deleteUrl = "/api/todos/" + clickedId;
+  $.ajax({
+    method: "DELETE",
+    url: deleteUrl
+  })
+    .then(function(data) {
+      todo.remove();
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
